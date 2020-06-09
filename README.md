93fbacaa7d6e6d9e636546442f1b34114a34f45f
Kenneth Mui <klmui@wisc.edu>
2020-06-09 04:13:45 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Sample-ng-Flask-MySQL-App
 Sample-ng-Flask-MySQL-App for personal reference.
+
+# Credits
+https://www.youtube.com/watch?v=6bL7n9aP6e0&t=29s
---
+++ mysql.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'db_tasks'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+CORS(app)
+
+@app.route('/api/tasks', methods=['GET'])
+def get_all_tasks():
+  cur = mysql.connection.cursor()
+  cur.execute("SELECT * FROM tasks")
+  # Returned value
+  rv = cur.fetchall()
+  return jsonify(rv)
+
+@app.route('/api/task', methods=['POST'])
+def add_task():
+  cur = mysql.connection.cursor()
+  title = request.get_json()['title']
+  # First parens is for columns, and second is the corresponding value
+  cur.execute("INSERT INTO tasks (title) VALUES ('" + str(title) + "')")
+  mysql.connection.commit()
+  result = {'title': title}
+  return jsonify({'result': result})
+
+@app.route('/api/task/<id>', methods=['PUT'])
+def update_task(id):
+  cur = mysql.connection.cursor()
+  title = request.get_json()['title']
+  
+
+if __name__ == '__main__':
+  app.run(debug=True)
